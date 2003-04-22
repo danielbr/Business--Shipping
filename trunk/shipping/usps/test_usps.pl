10841440d830b08ade9a8e704393d31dfc59f29a
db-ship <db-ship@fb931bad-f8dd-0310-ac82-dc66fac10115>
2003-04-22 23:24:26 +0000

    Actually add the test code this time...
    git-svn-id: file:///home/db/src/ship-svn-repo@7 fb931bad-f8dd-0310-ac82-dc66fac10115

---
+++ trunk/shipping/usps/test_usps.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+print "Testing USPS...\n\n";
+
+use Business::Ship;
+use Data::Dumper;
+
+my $shipment = new Business::Ship( 'USPS' );
+
+$shipment->set(
+	'user_id' => 'fdsjakl',
+	'password' => 'fdjkasl',
+);
+
+print Dumper( $shipment );
+print "\n";
+
+1;
