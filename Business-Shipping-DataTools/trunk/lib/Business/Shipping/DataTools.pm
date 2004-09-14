470853b0562cc3ea58dcc618003e5e36bff96025
db-ship <db-ship@fb931bad-f8dd-0310-ac82-dc66fac10115>
2004-09-14 22:54:18 +0000

    initial version
    git-svn-id: file:///home/db/src/ship-svn-repo@181 fb931bad-f8dd-0310-ac82-dc66fac10115

---
+++ Business-Shipping-DataTools/trunk/lib/Business/Shipping/DataTools.pm
@@ -0,0 +1,46 @@
+package Business::Shipping::DataTools;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Business::Shipping::DataTools - Convert tables from original format into usable format.  
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+This is an optional module.  It is used to update Business::Shipping::DataFiles.
+These tools convert the original source data obtained from shippers into a 
+format that is more palatable for Business::Shipping.
+
+=head1 AUTHOR
+
+Dan Browning, C<< <db@kavod.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-business-shipping-datatools@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically
+be notified of progress on your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2004 Dan Browning, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Business::Shipping::DataTools
