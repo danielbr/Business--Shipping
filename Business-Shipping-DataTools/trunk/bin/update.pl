d92a5008367cabde2d00bef607028d4ce845b6e4
db-ship <db-ship@fb931bad-f8dd-0310-ac82-dc66fac10115>
2004-12-04 02:41:42 +0000

    Fixes
    git-svn-id: file:///home/db/src/ship-svn-repo@203 fb931bad-f8dd-0310-ac82-dc66fac10115

--- Business-Shipping-DataTools/trunk/bin/update.pl
+++ Business-Shipping-DataTools/trunk/bin/update.pl
@@ -2,6 +2,13 @@ use strict;
 use warnings;
 
 use Business::Shipping::DataTools;
+#Business::Shipping::Logging->log_level( 'debug' );
+my $dt = Business::Shipping::DataTools->new( 
+    download => 1,
+    unzip => 1,    
+    convert => 1,
+);
 
-Business::Shipping::DataTools::do_update;
+$dt->do_update;
 
+#svn stat | grep \? | xargs rm
--- Business-Shipping-DataTools/trunk/lib/Business/Shipping/DataTools.pm
+++ Business-Shipping-DataTools/trunk/lib/Business/Shipping/DataTools.pm
@@ -2,6 +2,15 @@ package Business::Shipping::DataTools;
 
 use warnings;
 use strict;
+use Business::Shipping::Logging;
+#use Business::Shipping::Config;
+use Carp;
+use Fcntl ':flock';
+use File::Find;
+use File::Copy;
+use Config::IniFiles;
+use Archive::Zip qw(:ERROR_CODES);
+use English;
 
 =head1 NAME
 
@@ -21,8 +30,44 @@ This is an optional module.  It is used to update Business::Shipping::DataFiles.
 These tools convert the original source data obtained from shippers into a 
 format that Business::Shipping can use.
 
+
+use Business::Shipping::DataTools;
+
+my $dt = Business::Shipping::DataTools->new( update => 1 );
+$dt->do_update;
+
+
+
 =cut
 
+use Class::MethodMaker 2.0 
+    [
+        new    => [ qw/ -hash new / ],
+        scalar => [ qw/ update download unzip convert / ],
+    ];
+
+=item * do_update
+
+=cut
+
+sub do_update
+{
+    my ( $self ) = @_;
+    
+    if ( $self->update ) {
+        $self->download( 1 );
+        $self->unzip( 1 );
+        $self->convert( 1 );
+    }
+    
+    $self->do_download     if $self->download;
+    $self->do_unzip        if $self->unzip;
+    $self->do_convert_data if $self->convert;
+    
+    return;
+}
+    
+    
 =item * download_to_file( $url, $file )
 
 =cut
@@ -53,15 +98,11 @@ sub download_to_file
 
 # Extracts all files from the given zip
 
-=pod
-
 sub _unzip_file
 {
     my ( $zipName, $destination_directory ) = @_;
     $destination_directory ||= './';
     
-    use Archive::Zip qw(:ERROR_CODES);
-
     my $zip = Archive::Zip->new();
     my $status = $zip->read( $zipName );
     if ( $status != AZ_OK )  {
@@ -73,24 +114,19 @@ sub _unzip_file
     
     $zip->extractTree( '', $destination_directory );
     
+    debug( "Done extracting." );
+    
     return;
 }
-
-=cut
 
 =item * filename_only( $path )
 
 =cut
 
-sub filename_only
+sub filename_only 
 {
-    trace "( $_[0] )";
-    my $filename_with_extension = $_[0];
-    
-    my $filename_only = $filename_with_extension; 
-    $filename_only =~ s/\..+$//;
-    
-    return $filename_only;
+    ( $_ ) = $_[ 0 ] =~ /^(.+)\..+$/;
+    return $_;
 }
 
 =item * split_dir_file( $path )
@@ -120,6 +156,7 @@ sub remove_extension
     trace "( $file )";
     
     my $filename_only = filename_only( $file );
+    
     rename( $file, $filename_only );
     
     return $filename_only;
@@ -153,7 +190,7 @@ sub remove_windows_carriage_returns
     close(  IN                     );
     flock(  OUT,       LOCK_UN     );
     close(  OUT                    );
-    copy(   "$file.1", $file       );
+    File::Copy::copy(   "$file.1", $file       );
     unlink( "$file.1"              );
     
 
@@ -252,20 +289,19 @@ sub do_download
 {
     my ( $self ) = @_;
     my $data_dir = Business::Shipping::Config::data_dir();
-    debug( "data_dir = $data_dir" );
     
-    my $us_origin_rates_url = cfg()->{ ups_information }->{ us_origin_rates_url };
-    my $us_origin_zones_url = cfg()->{ ups_information }->{ us_origin_zones_url };
-    my $us_origin_rates_filenames = cfg()->{ ups_information }->{ us_origin_rates_filenames };
-    my $us_origin_zones_filenames = cfg()->{ ups_information }->{ us_origin_zones_filenames };
+    my $us_origin_rates_url = dtcfg()->{ ups_information }->{ us_origin_rates_url };
+    my $us_origin_zones_url = dtcfg()->{ ups_information }->{ us_origin_zones_url };
+    my $us_origin_rates_filenames = dtcfg()->{ ups_information }->{ us_origin_rates_filenames };
+    my $us_origin_zones_filenames = dtcfg()->{ ups_information }->{ us_origin_zones_filenames };
     
     for ( @$us_origin_zones_filenames ) {
         s/\s//g;
-        Business::Shipping::Util::download_to_file( "$us_origin_zones_url/$_", "$data_dir/$_" );
+        download_to_file( "$us_origin_zones_url/$_", "$data_dir/$_" );
     }
     for ( @$us_origin_rates_filenames ) {
         s/\s//g;
-        Business::Shipping::Util::download_to_file( "$us_origin_rates_url/$_", "$data_dir/$_" ) ;
+        download_to_file( "$us_origin_rates_url/$_", "$data_dir/$_" ) ;
     }
 }
 
@@ -275,24 +311,26 @@ sub do_download
 
 sub do_unzip
 {
+    my ( $self ) = @_;
+    
     for ( 
-            @{ cfg()->{ ups_information }->{ us_origin_rates_filenames } },
-            @{ cfg()->{ ups_information }->{ us_origin_zones_filenames } },
+            @{ dtcfg()->{ ups_information }->{ us_origin_rates_filenames } },
+            @{ dtcfg()->{ ups_information }->{ us_origin_zones_filenames } },
         )
     {
+        debug( "Going to unzip filename: $_" );
         #
         # Remove any leading spaces.
         #
         s/^\s//g;
-        my $filename_without_extension = Business::Shipping::Util::filename_only( $_ );
         my $data_dir = Business::Shipping::Config::data_dir();
-        #
-        # Disable splitting up the data.  I just want them in one big flat directory, for now.
-        #
-        #my $destionation_dir = "$filename_without_extension/";
-        my $destionation_dir = '';
-        debug3( "Going to unzip: $data_dir/$_ into directory $data_dir/$destionation_dir" );
-        Business::Shipping::Util::_unzip_file(  "$data_dir/$_", "$data_dir/$destionation_dir" )
+
+        # TODO: unzip different types of files into different directories.
+        my $src  = "$data_dir/$_";
+        my $dest = "$data_dir/";
+        
+        debug( "Going to unzip: $src into $dest" );
+        _unzip_file( $src, $dest );
     }
     
     return;
@@ -308,7 +346,7 @@ into one that Business::Shipping can use.
 sub do_convert_data
 {
     trace '()';
-    my $self = shift;
+    my ( $self ) = @_;
     
     #
     # * Find all *rate* csv files in the data directory (and sub-dirs)
@@ -322,10 +360,11 @@ sub do_convert_data
         #
         # Now, we do translate zone files.
         #
+        #debug( "File::Find found $_" );
         return if ( $File::Find::dir =~ /zone/i );
         return if ( $_ =~ /zone/i );
         return if ( $_ =~ /\d\d\d/ );
-        my $cvs_files_skip_regexes = cfg()->{ ups_information }->{ csv_files_skip_regexes };
+        my $cvs_files_skip_regexes = dtcfg()->{ ups_information }->{ csv_files_skip_regexes };
         foreach my $cvs_files_skip_regex ( @$cvs_files_skip_regexes ) {
             $cvs_files_skip_regex =~ s/\s//g;
             return if ( $_ eq $cvs_files_skip_regex );
@@ -342,48 +381,138 @@ sub do_convert_data
         # Ignore Dirs
         return unless ( -f $_ );
         
-        debug3( "$_\n" );
+        debug3( "File::Find adding $_\n" );
         
         push ( @files_to_process, $File::Find::name );
         return;
     };
     
-    find( $find_rates_files_sub, Business::Shipping::Config::data_dir() );
+    File::Find::find( $find_rates_files_sub, Business::Shipping::Config::data_dir() );
     
-    my $cannot_convert_at_this_time = cfg()->{ ups_information }->{ cannot_convert };
+    my $cannot_convert_at_this_time = dtcfg()->{ ups_information }->{ cannot_convert };
     
-    #
-    # add the data dir
-    #
-    my @temp;
+    # Add the data dir to each element, convert to a regular array.
+    
+    my @cannot_convert_at_this_time;
     for ( @$cannot_convert_at_this_time ) {
-        debug( "cannot convert $_" );
-        push @temp, Business::Shipping::Config::data_dir() . "/$_";
+        s/^\s+//g;
+        $_ = Business::Shipping::Config::data_dir() . "/$_";
+        push @cannot_convert_at_this_time, $_;
     }
-    $cannot_convert_at_this_time = \@temp;
+    
+    #debug( "cannot_convert_at_this_time = " . join( ', ', @cannot_convert_at_this_time ) );
+    #debug( "before grepping, files to process = " . join( ', ', @files_to_process ) );
+    
+    # Remove all elements of hte @files_to_process array that match any element 
+    # in the cannont_convert_at_this_time array.
+    
+    
+    
+    my @new;
+    foreach my $file_to_process ( @files_to_process ) {
+        my $match = 0;
+        foreach my $cannot_convert ( @cannot_convert_at_this_time ) {
+            if ( $file_to_process eq $cannot_convert ) {
+                $match = 1;
+            }
+        }
+        if ( ! $match ) {
+            push @new, $file_to_process;
+        }
+    }
+    @files_to_process = @new;
+    
+    #@files_to_process = grep { grep( $_, @cannot_convert_at_this_time ) } @files_to_process;
+
+    #debug( "after grepping, files to process = " . join( ', ', @files_to_process ) );
 
     #
     # Remove the files that we cannot convert at this time.
     #
-    @files_to_process = Business::Shipping::Util::remove_elements_of_x_that_are_in_y( \@files_to_process, $cannot_convert_at_this_time );
+    #@files_to_process = grep( !/^$cannot_convert_at_this_time$/, @files_to_process );
     
-    debug3( "files_to_process = " . join( "\n", @files_to_process ) );
+    if ( scalar @files_to_process <= 1 ) {
+        error "There are no files to process.  This probably means that File::Find could not "
+            . "find any files in the data/ directory becuase it was a symlink, did not exist, "
+            . "or was configured incorrectly.";
+        exit;
+    }
+        
+               
+    
+    #debug3( "files_to_process = " . join( "\n", @files_to_process ) );
     for ( @files_to_process ) {
-        Business::Shipping::Util::remove_windows_carriage_returns( $_ );
+        #debug( "Processing $_... press any key." );
+        #my $keypress = <STDIN>;
+        
+        debug "remove_windows_carriage returns...";
+        remove_windows_carriage_returns( $_ );
         convert_ups_rate_file( $_ );
         
-        $_ = Business::Shipping::Util::remove_extension( $_ );
+        $_ = remove_extension( $_ );
         $_ = rename_tables_that_start_with_numbers( $_);
         $_ = rename_tables_that_have_a_dash( $_ );
+        remove_misc( $_ );
     }
     #
     # Convert the ewwzone.csv file manually, since it is skipped, above.
     #
-    Business::Shipping::Util::remove_windows_carriage_returns( 
+    remove_windows_carriage_returns( 
         Business::Shipping::Config::data_dir() . '/ewwzone.csv' 
     );
     $self->convert_zone_file( 'ewwzone.csv' );
     
+}
+
+=head2 remove_misc()
+
+Removes dollar signs, some extra spaces, empty lines, and lines with just commas.
+
+=cut
+
+sub remove_misc
+{
+    trace '()';
+    my ( $path ) = @_;
+    
+    my $fh = get_fh( $path );
+    
+    # Speedy slurp
+    undef $INPUT_RECORD_SEPARATOR;
+    
+    debug "getting file contents...";
+    my $file_contents = <$fh>;
+    
+    $file_contents =~ s/\$//g;           # Dollar signs
+    $file_contents =~ s/(\d\d) \,/$1,/g; # Extra space
+    $file_contents =~ s/^,+$//g;         # Just commas
+    
+    # TODO: write new contents to file handle 
+    
+    debug "closing file...";
+    
+    close_fh( $fh );
+    
+    debug "writing new contents to file...";
+    
+    write_file( $path, $file_contents );
+    
+    return;
+}
+
+sub write_file
+{
+    my ( $path, $contents ) = @_;
+    
+    return unless $path and $contents;
+    
+    my $fh = get_fh( ">$path" );
+    
+    print $fh $contents;
+    
+    close_fh( $fh );
+    
+    return;    
 }
 
 =item * convert_zone_file
@@ -457,10 +586,17 @@ sub rename_tables_that_start_with_numbers
     my $path = shift;
     trace "( $path )";
     
+    if ( ! $path ) { error "No path"; return; }
+    
     $_ = $path;
     my $new_file = $_;
     
-    my ( $dir, $file ) = Business::Shipping::Util::split_dir_file( $path );
+    my ( $dir, $file ) = split_dir_file( $path );
+    
+    if ( not $file ) {
+        error( "Could not determine filename from path" );
+        return;
+    }
     
     if ( $file =~ /^\d/ ) {
         $new_file = "$dir/a_$file";
@@ -483,7 +619,7 @@ sub rename_tables_that_have_a_dash
     $_ = $path;
     my $new_file = $_;
     
-    my ( $dir, $file ) = Business::Shipping::Util::split_dir_file( $path );
+    my ( $dir, $file ) = split_dir_file( $path );
     
     if ( $file =~ /\-/ ) {
         $file =~ s/\-/\_/g;
@@ -504,27 +640,6 @@ sub auto_update
     my ( $self ) = @_;
     $self->update( 1 );
     $self->do_update();
-}
-
-=item * do_update
-
-=cut
-
-sub do_update
-{
-    my ( $self ) = @_;
-    
-    if ( $self->update ) {
-        $self->download( 1 );
-        $self->unzip( 1 );
-        $self->convert( 1 );
-    }
-    
-    $self->do_download()         if $self->download;
-    $self->do_unzip()             if $self->unzip;
-    $self->do_convert_data()    if $self->convert;
-    
-    return;
 }
 
 =item * get_fh( $filename )
@@ -554,6 +669,37 @@ sub close_fh
     
     return;
 }
+
+
+use constant DEFAULT_DT_SUPPORT_FILES_DIR => '.';
+
+my $dt_support_files_dir;
+my $dt_config_file;
+
+# Try the current directory first.
+
+if ( -f 'config/DataTools.ini' ) {
+    $dt_support_files_dir = '.';
+}
+
+# Then try environment variables
+
+$dt_support_files_dir ||= $ENV{ BUSINESS_SHIPPING_SUPPORT_FILES };
+
+# Then fall back on the default.
+
+$dt_support_files_dir ||= DEFAULT_DT_SUPPORT_FILES_DIR;
+
+$dt_config_file = "$dt_support_files_dir/config/DataTools.ini";
+
+if ( ! -f $dt_config_file ) {
+    die "Could not open data tools configuration file: $dt_config_file: $!";
+}
+
+tie my %dtcfg, 'Config::IniFiles', (      -file => $dt_config_file );
+my $dtcfg_obj = Config::IniFiles->new(    -file => $dt_config_file );
+
+sub dtcfg { return \%dtcfg; }
 
 =head1 AUTHOR
 
