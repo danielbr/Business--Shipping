c42053381f666a97cd9204f002f8400e243319cc
db-ship <db-ship@fb931bad-f8dd-0310-ac82-dc66fac10115>
2005-08-02 21:19:29 +0000

    Test updates
    git-svn-id: file:///home/db/src/ship-svn-repo@252 fb931bad-f8dd-0310-ac82-dc66fac10115

--- trunk/shipping/t/manual/UPS-Offline-error.pl
+++ trunk/shipping/t/manual/UPS-Offline-error.pl
@@ -5,15 +5,15 @@ use warnings;
 
 use Business::Shipping;
 
-#Business::Shipping->log_level( 'debug' );
+Business::Shipping->log_level( 'debug' );
 
 my $rate_request = Business::Shipping->rate_request( shipper => 'UPS_Offline' );
 
 my $results = $rate_request->submit(
-    service    => '1DA',
-    weight     => '100',
-    from_zip   => '98682',
-    to_zip     => '98270',
+    service        => '2DA',
+    weight        => 20,
+    from_zip    => '98682',
+    to_zip        => '97015',
 ) or die $rate_request->user_error();
 
 print "offline = " . $rate_request->total_charges() . "\n";
---
+++ trunk/shipping/t/manual/UPS-Online-error.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Business::Shipping;
+
+#Business::Shipping->log_level( 'debug' );
+
+my $rate_request = Business::Shipping->rate_request( shipper => 'UPS_Online' );
+
+my $FROM_ZIP = '01351'; # Montague, MA
+my $to_zip = '94110';   # San Francisco, CA
+my $package_weight = 5;
+
+my $results = $rate_request->submit(
+                shipper         => 'UPS_Online',
+                service         => 'GNDCOM', 
+                from_zip        => $FROM_ZIP,
+                to_zip          => $to_zip,
+                weight          => $package_weight,
+                user_id         => $ENV{ UPS_USER_ID },
+                password        => $ENV{ UPS_PASSWORD },
+                access_key      => $ENV{ UPS_ACCESS_KEY },
+                pickup_type     =>  'daily',
+                from_country    => 'US',
+                to_country      => 'US',
+                to_residential  => 1,
+) or die $rate_request->user_error();
+
+use Data::Dumper;
+print "UPS_Online: " . $rate_request->total_charges() . "\n";
+
+# Should be 6.51 for non-residential.
