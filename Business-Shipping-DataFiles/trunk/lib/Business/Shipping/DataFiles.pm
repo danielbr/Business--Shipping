8ad309240abd9f1643bf6f2a9697f773d823c688
db-ship <db-ship@fb931bad-f8dd-0310-ac82-dc66fac10115>
2005-08-14 16:59:33 +0000

    Prep for release
    git-svn-id: file:///home/db/src/ship-svn-repo@291 fb931bad-f8dd-0310-ac82-dc66fac10115

--- Business-Shipping-DataFiles/trunk/lib/Business/Shipping/DataFiles.pm
+++ Business-Shipping-DataFiles/trunk/lib/Business/Shipping/DataFiles.pm
@@ -9,11 +9,11 @@ Business::Shipping::DataFiles - Offline rate tables for Business::Shipping
 
 =head1 VERSION
 
-Version 1.00
+Version 1.01
 
 =cut
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 1;
 
